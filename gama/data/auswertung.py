fe3fcc9eded37521b9cde2418635d0efdde16c01
Dominik Wille <dominik.wille@fu-berlin.de>
2013-12-11 14:47:57 +0100

    kal fits

--- gama/data/auswertung.py
+++ gama/data/auswertung.py
@@ -13,7 +13,7 @@ x = []
 y = []
 ye = []
 i = 0
-with open('60cokal.Spe', 'rb') as f:
+with open('22nacal.Spe', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
         s = str(row[0]).strip()
@@ -26,19 +26,20 @@ x = np.asarray(x)
 y = np.asarray(y)
 
 
-x_ = x[5770:6500]
-y_ = y[5770:6500]
+x_ = x[5200:6100]
+y_ = y[5200:6100]
 
 mean = 6000
 sigma = 250
 
 popt,pcov = curve_fit(gaus,x_,y_,p0=[1,mean,sigma])
 
+d_x0 = pcov[1,1]
 a = popt[0]
 x0 = popt[1]
 sigma = popt[2]
 
-text = r'$x_0 =\, ' + str(round(x0, 2)) + ';\,\, \sigma =' + str(abs(round(sigma, 2))) + '$'
+text = r'$x_0 =\, ' + str(round(x0, 2)) + '\pm ' + str(round(np.sqrt(d_x0), 2))  + '$'#;\,\, \sigma =' + str(abs(round(sigma, 2))) + '$'
 plt.text(x0, a * 1.2 + 27, text, horizontalalignment='center')#, verticalalignment='bottom' , rotation='vertical')
 
 plt.errorbar(x, y, xerr=0.1, yerr=0.01)
